0dac6c1700e28a00628b9c4cb32c6f25356b7aad
igna <ignacioborraz@hotmail.com>
2024-05-22 10:23:04 -0300

    router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { HomeComponent } from './views/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      //{ path: 'new', component: FormComponent },
+      { path: 'new', loadComponent: ()=> import('./views/form-view/form-view.component').then(res=>res.FormViewComponent) },
+      //{ path: '**', component: NotFoundComponent },
+      { path: '**', loadComponent: ()=> import('./views/not-found/not-found.component').then(res=>res.NotFoundComponent)}
+    ],
+  },
+];
---
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-main-layout',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './main-layout.component.html',
+  styles: ``
+})
+export class MainLayoutComponent {
+
+}
